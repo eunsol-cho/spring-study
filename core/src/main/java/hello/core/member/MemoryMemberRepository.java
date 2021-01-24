0f63d885026d08e01743b522f71756cd167f7336
eunsolJo <eunsol704@gmail.com>
2021-01-24 18:24:14 +0900

    스프링 핵심 원리:예제 만들기

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// opt + enter : 자동완성
+// 인터페이스와 구현체는 다른 패키지에 넣는게 좋다.
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // 동시성 이슈로 ConcurrentHashMap을 쓰는것이 좋다.
+    
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
