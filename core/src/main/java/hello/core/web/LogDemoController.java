3e2ed9974ce22798200b543552829ead3572ed32
eunsolJo <eunsol704@gmail.com>
2021-02-06 00:19:47 +0900

    prj/core-웹스코프

---
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemeService logDemeService;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        //MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemeService.logic("testId");
+        return "OK";
+    }
+}
